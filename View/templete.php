6eca796ac2fd47ed3f2e2ffe9cd9138fa012b9b4
Emam Hossain <eh.tamim1708@gmail.com>
2023-12-22 09:40:30 +0600

    changes

---
+++ View/templete.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../css/mycss.css">
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">About</a></li>
+  <li></li>
+  <li><a href="#about">Login</a></li>
+</ul>
+
+</body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include("view/templete.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="css/mycss.css">
+</head>
+<body>
+</body>
+</html>
+
+
